7c992ec37480cab0740c88cffd451db9ad2f1ff2
NHelixo <sasasas123sasasasa@gmail.com>
2024-05-24 18:00:44 +0300

    message

---
+++ test_image_button.py
@@ -0,0 +1,21 @@
+from kivy.app import App
+from kivy.uix.relativelayout import RelativeLayout
+from kivy.uix.button import Button
+from kivy.uix.image import Image
+from kivy.uix.boxlayout import BoxLayout
+
+
+class MyApp1(App):
+    def build(self):
+        layout = RelativeLayout()
+
+        button = Button(text = "fff", background_normal='E:/python/cliker game/pictures/button.png', background_down= 'E:/python/cliker game/pictures/button_down.png')
+
+        layout.add_widget(button)
+
+        return layout
+
+
+
+if __name__ == '__main__':
+    MyApp1().run()
